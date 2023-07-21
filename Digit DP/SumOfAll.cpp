7bc571384fdca512986b853440a246c3735ae13f
Tran Gia Huy <121950151+Akiraa-dev@users.noreply.github.com>
2023-07-21 21:06:54 +0700

    Update SumOfAll.cpp

--- Digit DP/SumOfAll.cpp
+++ Digit DP/SumOfAll.cpp
@@ -13,8 +13,7 @@
 typedef long long ll;
 typedef long double ldb;
 using namespace std;
-/**------Ideas---------
----------------------**/
+
 const ldb PI = 3.14159265358979323846264338327950288419716939937510582097494459230;
 const ll INF = 1e18;
 const ll MOD = 1e9 + 7;
